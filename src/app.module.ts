cc645b0fe46bed94137d702a76e51eaed4798ca1
Bryan Brenes <bryanbrenes1@gmail.com>
2025-07-18 20:39:25 -0600

    Add aws sdk

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import awsConfig from './config/aws.config';
 import { NxModule } from './nx/nx.module';
 
 @Module({
-  imports: [NxModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true, load: [awsConfig] }),
+    NxModule,
+  ],
   controllers: [],
   providers: [],
 })
--- src/nx/nx.service.ts
+++ src/nx/nx.service.ts
@@ -1,7 +1,16 @@
+import { S3Client } from '@aws-sdk/client-s3';
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class NxService {
+  private s3Client: S3Client;
+
+  constructor(private configService: ConfigService) {
+    const region = this.configService.get<string>('aws.region');
+    this.s3Client = new S3Client({ region });
+  }
+
   async putCacheItem(hash: string, value: string): Promise<string> {
     return `Cache item for hash ${hash} updated with value ${value}`;
   }
